47ffe98f9c4532f858486c4bee565e77ac1294d0
Darrell <ashilpadarrel649@gmail.com>
2023-04-20 10:41:51 +0700

    Latihan 1

---
+++ Modul 8/latihan1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Latihan Program PHP</title>
+    </head>
+    <body>
+        <?php
+            echo "kode php di sini";
+        ?>
+
+        <p>Latihan 1</p>
+
+        <?php 
+            echo "kode php di sini";
+        ?>
+
+        <p> Latihan <?php echo "PHP" ?> di HTML</p>
+    </body>
+</html>
